59f127b4bdc36934aff718e399aa685b6f1d214a
AlejoGm <81264011+AlejoGm@users.noreply.github.com>
2025-04-24 20:37:23 -0300

    apply git ignore

--- Debug/API/src/API_matrix_level.d
+++
@@ -1,3 +0,0 @@
-API/src/API_matrix_level.o: ../API/src/API_matrix_level.c \
- D:/User/Documentos/GitHub/CESE/PdM_workspace_/TP-final/API/incl/API_matrix_level.h
-D:/User/Documentos/GitHub/CESE/PdM_workspace_/TP-final/API/incl/API_matrix_level.h:
